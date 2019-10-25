d6f0ee3531d4d3dfe1a3409ff93074eda1a7f2e3
Safia MSELLEK-ISMAILI <sms@cnam-lyon.peda>
2019-10-25 16:52:21 +0200

    annotation dans routes.yaml et AdminController

--- src/Controller/TravelerController.php
+++ src/Controller/TravelerController.php
@@ -6,9 +6,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TravelerController extends AbstractController
 {
+  /**
+   * @Route ("/", name="home")
+   */
   public function index(Environment $twig)
   {
     $content = $twig->render('Traveler/index.html.twig');
