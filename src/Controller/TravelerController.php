3826c8fba0d4c3991832a5cbf1c627c4de861186
Safia MSELLEK-ISMAILI <sms@cnam-lyon.peda>
2019-10-25 12:29:23 +0200

    creation d'un controller et une page d'acceuil avec un welcome!

--- src/Controller/TravelerController.php
+++ src/Controller/TravelerController.php
@@ -4,12 +4,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 class TravelerController
 {
-  public function index()
+  public function index(Environment $twig)
   {
-    $content = "Welcome !";
+    $content = $twig->render('Traveler/index.html.twig');
 
     return new Response($content);
   }
